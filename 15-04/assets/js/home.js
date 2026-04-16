71c9622e4065aefed902bdb66030a3af422e0279
Eduarda C. <dossantosec@outlook.com>
2026-04-16 19:57:53 -0300

    feat: altera função para que alterne os textos
    pequenas mudanças nos textos;
    melhoria na indentação;
    remove espaçamentos

--- 15-04/assets/js/home.js
+++ 15-04/assets/js/home.js
@@ -1,20 +1,27 @@
-// Pegar cada elemento que vamos usar
-// Entre no documento HTML, e pegue o elemento que tem o id "titulo" e o id "btn"
-const titulo = document.getElementById('titulo');
-const botao = document.getElementById('btn');
-
+// Atribuir variáveis para os elementos
+// "Entramos" no documento HTML, e "procuramos" o elemento pelo seu id. 
+const titulo = document.getElementById("titulo");
+const botao = document.getElementById("btn");
+// Troca cor de um elemento
 function trocaCor() {
-    titulo.style.color = "red";
+  titulo.style.color = "red";
 }
-
+// Imprime um aviso
 function mensagem() {
-    alert("Mensagem");
+  alert("Mensagem");
 }
-
+// Alterna os conteúdos do texto a cada clique
 function trocaMensagem() {
-    titulo.textContent = "Texto trocado";
+  texto = titulo.textContent;
+  switch (texto) {
+    case "Texto":
+      titulo.textContent = "Texto trocado!";
+      break;
+    default:
+      titulo.textContent = "Texto";
+      break;
+  }
 }
-
 // Adiciona um evento ao botão
 // Um evento é uma ação do usuário
 // "addEventListener" precisa de dois argumentos: o primeiro é uma ação, que dispara uma função; o segundo é qual função deve ser disparada.
