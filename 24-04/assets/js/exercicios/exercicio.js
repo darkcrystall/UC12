d5e111ad0957a008dead943772633fc3211198b9
Eduarda C. <dossantosec@outlook.com>
2026-04-25 15:23:03 -0300

    refactor: melhora a manipulação das imagens e botões
    corrige a ideia anterior;
    adiciona mais comentários;
    adiciona exemplo do professor

--- 24-04/assets/js/exercicios/exercicio.js
+++ 24-04/assets/js/exercicios/exercicio.js
@@ -1,8 +1,8 @@
-/*
-// Testar essa ideia depois 
-const images = document.querySelectorAll(".image")
+// Lista com todas imagens
+const images = document.querySelectorAll(".image");
+// Lista com todos botões
 const buttons = document.querySelectorAll(".btn-action");
-*/
+/*
 // Elementos da primeira imagem
 const btnGalaxy = document.getElementById("btn-galaxy");
 const imgGalaxy = document.getElementById("image-galaxy");
@@ -13,38 +13,51 @@ const imgPlanets = document.getElementById("image-planets");
 const btnSpace = document.getElementById("btn-space");
 const imgSpace = document.getElementById("image-space");
 // Primeiro botão
-btnGalaxy.addEventListener("click", () => {
-  imgGalaxy.classList.toggle("active");
-  btnGalaxy.textContent == "Mostrar imagem"
-    ? (btnGalaxy.textContent = "Esconder imagem")
-    : (btnGalaxy.textContent = "Mostrar imagem");
+// btnGalaxy.addEventListener("click", () => {
+//   imgGalaxy.classList.toggle("active");
+//   btnGalaxy.textContent == "Mostrar imagem"
+//     ? (btnGalaxy.textContent = "Esconder imagem")
+//     : (btnGalaxy.textContent = "Mostrar imagem");
+// });
+// // Segundo botão
+// btnPlanets.addEventListener("click", () => {
+//   imgPlanets.classList.toggle("active");
+//   btnPlanets.textContent == "Mostrar imagem"
+//     ? (btnPlanets.textContent = "Esconder imagem")
+//     : (btnPlanets.textContent = "Mostrar imagem");
+// });
+// // Terceiro botão
+// btnSpace.addEventListener("click", () => {
+//   imgSpace.classList.toggle("active");
+//   btnSpace.textContent == "Mostrar imagem"
+//     ? (btnSpace.textContent = "Esconder imagem")
+//     : (btnSpace.textContent = "Mostrar imagem");
+// });
+*/
+// Exemplo do professor para tornar mais fácil (usando forEach)
+/*
+buttons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const image = images[index];
+    image.classList.toggle("active");
+    // Aqui ele havia usado if else, mas não faz diferença
+    button.textContent =
+      button.textContent == "Mostrar imagem"
+        ? "Esconder imagem"
+        : "Mostrar imagem";
+  });
 });
-// Segundo botão
-btnPlanets.addEventListener("click", () => {
-  imgPlanets.classList.toggle("active");
-  btnPlanets.textContent == "Mostrar imagem"
-    ? (btnPlanets.textContent = "Esconder imagem")
-    : (btnPlanets.textContent = "Mostrar imagem");
-});
-// Terceiro botão
-btnSpace.addEventListener("click", () => {
-  imgSpace.classList.toggle("active");
-  btnSpace.textContent == "Mostrar imagem"
-    ? (btnSpace.textContent = "Esconder imagem")
-    : (btnSpace.textContent = "Mostrar imagem");
-});
-/* 
-// Testar essa ideia depois
-for (let i = 0; i < buttons.length; i++) {
-  for (let i = 0; i < images.length; i++) {
-    buttons[i].addEventListener("click", () => {
-      if (buttons[i] === images[i]) {
-        images[i].classList.toggle("active");
-        buttons[i].textContent == "Mostrar imagem"
-          ? (buttons[i].textContent = "Esconder imagem")
-          : (buttons[i].textContent = "Mostrar imagem");
+*/
+// Corrigido essa ideia (mais complexo, mas facilita e funciona da mesma forma)
+for (let btnIndex = 0; btnIndex < buttons.length; btnIndex++) {
+  for (let imgIndex = 0; imgIndex < images.length; imgIndex++) {
+    buttons[btnIndex].addEventListener("click", () => {
+      if (btnIndex === imgIndex) {
+        images[imgIndex].classList.toggle("active");
+        buttons[btnIndex].textContent == "Mostrar imagem"
+          ? (buttons[btnIndex].textContent = "Esconder imagem")
+          : (buttons[btnIndex].textContent = "Mostrar imagem");
       }
     });
   }
 }
-*/
