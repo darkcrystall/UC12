27b63cc84e1c50f7f0a12c5224f552d7472cf52a
Eduarda C. <dossantosec@outlook.com>
2026-05-07 13:59:02 -0300

    feat: melhora exemplo

--- 05-05/assets/js/index.js
+++ 05-05/assets/js/index.js
@@ -1,7 +1,10 @@
 const div = document.getElementById("container");
 const btn = document.getElementById("btn");
-// Criar novos elementos e fazê-los aparecer no HTML
-const novo = document.createElement("p");
-novo.textContent = "Texto criado";
-// Depois de criado o elemento, precisamos dizer onde ele será posicionado
-div.appendChild(novo);
+// Adiciona um evento de clique, que ao clicar, irá adicionar um parágrafo toda vez
+btn.addEventListener("click", () => {
+  // Criar novos elementos e fazê-los aparecer no HTML
+  const novo = document.createElement("p");
+  novo.textContent = "Texto criado";
+  // Depois de criado o elemento, precisamos dizer onde ele será posicionado
+  div.appendChild(novo);
+});
